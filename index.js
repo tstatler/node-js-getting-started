47bf9656e877dbb0016277e74c4658d6bd67c57b
tstatler <tstatler@cisco.com>
2022-04-10 16:56:59 -0700

    added post

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const { appendFile } = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+
+const app = express()
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.post('/', function(req, res)) {
+  res.send("POSTED!")
+}
